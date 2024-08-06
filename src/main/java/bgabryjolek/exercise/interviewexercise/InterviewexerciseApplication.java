66834a0e82f28f4c721bb6855eaf7cea30b55b70
Delris <58948538+Delris@users.noreply.github.com>
2024-08-06 20:27:40 +0200

    Add files via upload

---
+++ src/main/java/bgabryjolek/exercise/interviewexercise/InterviewexerciseApplication.java
@@ -0,0 +1,13 @@
+package bgabryjolek.exercise.interviewexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bgabryjolek/exercise/interviewexercise/InterviewexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package bgabryjolek.exercise.interviewexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
